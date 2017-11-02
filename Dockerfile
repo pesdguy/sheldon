a51b068460d8bb121be7a645abd9f862aa465aa0
Prabhjit Singh <prabhjitsingh@Prabhjits-MacBook-Air.local>
2017-11-02 22:30:32 +0530

    First Commit

---
+++ Dockerfile
@@ -0,0 +1,80 @@
+FROM alpine
+
+# Setup demo environment variables
+ENV HOME=/root \
+        DEBIAN_FRONTEND=noninteractive \
+        LANG=en_US.UTF-8 \
+        LANGUAGE=en_US.UTF-8 \
+        LC_ALL=C.UTF-8 \
+        DISPLAY=:0.0 \
+        DISPLAY_WIDTH=1024 \
+        DISPLAY_HEIGHT=768
+
+# x11vnc is in testing repo
+RUN echo "http://dl-3.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
+
+# Install git, supervisor, VNC, & X11 packages
+RUN apk --update --upgrade add --no-cache firefox-esr \
+        openrc \
+        dbus \
+        bash \
+        openssh \
+        fluxbox \
+        git \
+        socat \
+        supervisor \
+        x11vnc \
+        xterm \
+        xvfb \
+        python \
+        py-pip \
+        py-setuptools \
+        libx11 \
+        python2-dev \
+        mysql-client \
+        py-mysqldb \
+        py-lxml \
+        py-dateutil \
+        python py-pip curl unzip libexif udev chromium chromium-chromedriver xvfb && \
+        pip install selenium && \
+        pip install pyvirtualdisplay \
+        requests \
+        boto3 \
+        awscli \
+        tendo \
+        bottlenose \
+ && sed -i s/#PermitRootLogin.*/PermitRootLogin\ yes/ /etc/ssh/sshd_config \
+  && echo "root:root" | chpasswd \
+  && rm -rf /var/cache/apk/*
+RUN sed -ie 's/#Port 22/Port 22/g' /etc/ssh/sshd_config
+RUN sed -ri 's/#HostKey \/etc\/ssh\/ssh_host_key/HostKey \/etc\/ssh\/ssh_host_key/g' /etc/ssh/sshd_config
+RUN sed -ir 's/#HostKey \/etc\/ssh\/ssh_host_rsa_key/HostKey \/etc\/ssh\/ssh_host_rsa_key/g' /etc/ssh/sshd_config
+RUN sed -ir 's/#HostKey \/etc\/ssh\/ssh_host_dsa_key/HostKey \/etc\/ssh\/ssh_host_dsa_key/g' /etc/ssh/sshd_config
+RUN sed -ir 's/#HostKey \/etc\/ssh\/ssh_host_ecdsa_key/HostKey \/etc\/ssh\/ssh_host_ecdsa_key/g' /etc/ssh/sshd_config
+RUN sed -ir 's/#HostKey \/etc\/ssh\/ssh_host_ed25519_key/HostKey \/etc\/ssh\/ssh_host_ed25519_key/g' /etc/ssh/sshd_config
+RUN /usr/bin/ssh-keygen -A
+RUN ssh-keygen -t rsa -b 4096 -f  /etc/ssh/ssh_host_key
+RUN rc-update add sshd && \
+    rc-status          && \
+    touch /run/openrc/softlevel  && \
+    rc-service sshd start
+# Clone noVNC from github
+RUN git clone https://github.com/kanaka/noVNC.git /root/noVNC \
+        && git clone https://github.com/kanaka/websockify /root/noVNC/utils/websockify \
+        && rm -rf /root/noVNC/.git \
+        && rm -rf /root/noVNC/utils/websockify/.git \
+        && apk del git
+
+COPY supervisord.conf /etc/supervisor/conf.d/supervisord.conf
+COPY ebay /
+COPY geckodriver /
+#COPY password /
+# Modify the launch script 'ps -p'
+RUN sed -i -- "s/ps -p/ps -o pid | grep/g" /root/noVNC/utils/launch.sh
+RUN chmod +x geckodriver
+
+EXPOSE 8080
+
+CMD ["/usr/bin/supervisord", "-c", "/etc/supervisor/conf.d/supervisord.conf"]
+
+
---
+++ ebay/Ebayorderid.py
@@ -0,0 +1,155 @@
+import sys
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import time
+from zeep import Client
+import json
+
+
+processOrderUrl = ''
+with open('setting.json') as setting_file:
+	settingsdata = json.load(setting_file)
+	processOrderUrl = settingsdata["ProcessOrderurl"]
+	cardno=settingsdata["cardno"]
+	Expd=settingsdata["Expd"]
+	cvv=settingsdata[“cvv”]
+	Uname=settingsdata["Uname"]
+	Pass=settingsdata["Pass"]
+	paypal_Un=settingsdata["paypal_Un"]
+	paypal_pass=settingsdata["paypal_pass"]
+    
+    
+def GetOrderInfo():
+    client = Client('http://staging.esupplybox.com/api/soap/?wsdl')
+
+    clientSession = client.service.login("test", "tester")
+    resultFilter =  ""
+    result = client.service.call(clientSession, 'neworders.list', str(resultFilter))
+    print result
+    jsonData = json.loads(result)
+    for order in jsonData:
+        Entity_id=order["entity_id"]
+        Itemid=order["itemid"]
+        orderdate=order["orderdate"]
+        Shipname=order["buyer_name"]
+        Split_Name=Shipname.split()
+        First_Name=Split_Name[0]
+        Last_Name=Split_Name[1]
+        Ship_address1=order["address_line_1"]
+        Ship_address2=order["address_line_2"]
+        State=order["state"]
+        City=order["city"]
+        Country=order["country"]
+        Telephone=order["telephone"]
+        Country=order["country"]
+        Zipcode=order["zipcode"]
+        Ebay_Item_id=order["selectedebayitemid"]
+        SubmitOrder(Ebay_Item_id,First_Name,Last_Name,Ship_address1,Ship_address2,City,State,Telephone,Country,Zipcode)
+
+
+def SubmitOrder(ebayitemid,First_Name,Last_Name,Ship_address1,Ship_address2,city,state,Telephone,country,Zipcode):
+	driver=webdriver.Firefox()
+	url = processOrderUrl + str(ebayitemid)
+	driver.get(url)
+	main_window = driver.window_handles[0]
+	time.sleep(20)
+	Buybutton=driver.find_element_by_id("binBtn_btn")
+	Buybutton.click()
+	#Buybutton=driver.find_element_by_xpath("html/body/div[3]/div[3]/div[3]/div[2]/div[1]/div/div[3]/div[2]/div[3]/div/div[1]/a")
+	#Buybutton.click()
+	time.sleep(20)
+	Username=driver.find_element_by_id("userid")
+	Username.send_keys(Uname)
+	Password=driver.find_element_by_id("pass")
+	Password.send_keys(Pass)
+	Signin=driver.find_element_by_id("sgnBt")
+	Signin.click()
+	time.sleep(10)
+	Commitbutton=driver.find_element_by_id("but_v4-2")
+	Commitbutton.click()
+	time.sleep(20)
+	Paynowbutton=driver.find_element_by_id("but_v4-1")
+	Paynowbutton.click()
+	time.sleep(100)
+	AddressChange=driver.find_element_by_xpath(".//*[@id='sa-change-link']/a")
+	AddressChange.click()
+	time.sleep(30)
+	Add_address=driver.find_element_by_link_text("Add a new address")
+	Add_address.click()
+	time.sleep(10)
+	CountryOrRegion=Select(driver.find_element_by_id("af-country"))
+	CountryOrRegion.select_by_visible_text(country)
+	time.sleep(2)
+	ContactFName=driver.find_element_by_id("af-first-name")
+	ContactFName.send_keys(First_Name)
+	time.sleep(1)
+	ContactLName=driver.find_element_by_id("af-last-name")
+	ContactLName.send_keys(Last_Name)
+	StreetAddress1=driver.find_element_by_id("af-address1")
+	StreetAddress1.send_keys(Ship_address1)
+	time.sleep(1)
+	StreetAddress2=driver.find_element_by_id("af-address2")
+	StreetAddress2.send_keys(Ship_address2)
+	time.sleep(1)
+	City=driver.find_element_by_id("af-city")
+	City.send_keys(city)
+	time.sleep(1)
+	State=Select(driver.find_element_by_id("af-state"))
+	State.select_by_visible_text(state)
+	time.sleep(1)
+	zipc=driver.find_element_by_id("af-zip")
+	zipc.send_keys(Zipcode)
+	time.sleep(1)
+	Phoneno=driver.find_element_by_xpath(".//*[@id='address-fields']/div[6]/span/input")
+	Phoneno.send_keys(Telephone)
+	time.sleep(1)
+	Add=driver.find_element_by_xpath(".//*[@id='address-fields-ctr']/div[2]/div[3]/button")
+	Add.click()
+	time.sleep(10)
+	ConfirmAdd=driver.find_element_by_id("recommended-addr-btn")
+	ConfirmAdd.click()
+	time.sleep(20)
+	Paypal=driver.find_element_by_id("PAYPAL")
+	Paypal.click()
+	time.sleep(5)
+	child_window = driver.window_handles[1]
+	driver.switch_to_window(child_window)
+	time.sleep(30)
+	Paypal_Username=driver.find_element_by_id("email")
+	Paypal_Username.send_keys(paypal_Un)
+	time.sleep(1)
+	Paypal_Password=driver.find_element_by_id("password")
+	Paypal_Password.send_keys(paypal_pass)
+	time.sleep(1)
+	Paypal_Login=driver.find_element_by_id("btnLogin")
+	Paypal_Login.click()
+	time.sleep(30)
+	rememberme=driver.find_element_by_id("declineRememberMe")
+	rememberme.click()
+	time.sleep(30)
+	Entercardnumb=driver.find_element_by_id("cc")
+	Entercardnumb.send_keys(cardno)
+	time.sleep(5)
+	Expirydate=driver.find_element_by_id("expiry_value")
+	Expirydate.send_keys(Expd)
+	time.sleep(5)
+	EnterCVV=driver.find_element_by_id("cvv")
+	EnterCVV.send_keys(cvv)
+	time.sleep(5)
+	proceed=driver.find_element_by_id("proceedButton")
+	proceed.click()
+	time.sleep(5)
+	driver.switch_to_window(main_window)
+	updateOrderStatus(ebayitemid)
+	except Exception:
+	driver.close()
+	pass
+	
+
+
+GetOrderInfo()
+
+
+
+
+
