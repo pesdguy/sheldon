f853cabd72f99db4e2545c587ca5b6dd4259002f
Prabhjit Singh <pesdguy@yahoo.co.in>
2017-11-13 17:33:29 +0530

    update orders using php arguments

--- ebay/updateorder.php
+++ ebay/updateorder.php
@@ -5,16 +5,20 @@ error_reporting(E_ALL);
 $client = new SoapClient('http://staging.esupplybox.com/api/soap/?wsdl', array('trace' => 1));
 $session = $client->login('test', 'tester');
 
-$params = array(
-    'item_id' => 9059,
-    'order_id' => 7368,
-    'ebay_transaction_id' => '-',
-    'trackig_id' => 'Issue In Item Page',
-    'tracking_url' => 'cyz',
-);
+if(defined('STDIN'))  {
+    $params = array(
+        'item_id' => $argv[1],
+        'order_id' => $argv[2],
+        'ebay_transaction_id' => $argv[3],
+        'trackig_id' => $argv[4],
+        'tracking_url' => $argv[5],
+    );
 
-$result = $client->call($session, 'neworders.update', $params);
-echo "<pre>";
-print_r($result);
+    $result = $client->call($session, 'neworders.update', $params);
+    echo "<pre>";
+    print_r($result);
+} else  {
+    echo 'Argument Not Found';
+}
 exit();
 ?>
